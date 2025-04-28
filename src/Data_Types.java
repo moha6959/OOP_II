0cd9c32bb44baf96edb29bd61303545fde1ac0db
moha6959 <mohamedekaden576@gmail.com>
2025-04-28 12:18:35 +0300

    Completed challenge 2 showing different data types

---
+++ src/Data_Types.java
@@ -0,0 +1,21 @@
+public class Data_Types {
+        public static void main(String[] args) {
+
+            int number = 25;
+            double price = 19.99;
+            char letter = 'A';
+            boolean isJavaFun = true;
+
+
+            String message = "Hello, Java!";
+
+
+            System.out.println("Integer value: " + number);
+            System.out.println("Double value: " + price);
+            System.out.println("Character value: " + letter);
+            System.out.println("Boolean value: " + isJavaFun);
+            System.out.println("String value: " + message);
+        }
+    }
+
+
