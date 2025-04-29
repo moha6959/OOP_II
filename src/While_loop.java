356cd9f014a8dc47f6775cda28ac1f6b10cfef88
moha6959 <mohamedekaden576@gmail.com>
2025-04-29 13:14:23 +0300

    Completed challenge 5 A code to show while loop

---
+++ src/While_loop.java
@@ -0,0 +1,11 @@
+public class While_loop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
+
+        }
+    }
+}
