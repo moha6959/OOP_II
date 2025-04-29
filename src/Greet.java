a22d663a5e7ca6bb4c05d6e40172354f945dd145
moha6959 <mohamedekaden576@gmail.com>
2025-04-29 13:29:05 +0300

    Completed challenge 1 A function calling code

--- src/Greet.java
+++ src/Greet.java
@@ -1,30 +1,9 @@
-import java.util.Scanner;
 
 public class Greet {
-
-    private String username;
-
-    public Greet(String username) {
-        this.username = username;
-    }
-
-    public String sayHello() {
-        return "Hello, " + username + "!";
-    }
-
-    public static void startGreeting() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String nameInput = scanner.nextLine();
-
-        Greet user = new Greet(nameInput);
-        System.out.println(user.sayHello());
-
-        scanner.close();
-    }
-
-    public static void main(String[] args) {
-        startGreeting();
+       void greetings(){
+           System.out.println("Hi, how are you doing");
+       }
+    public static void main(String[] args){
+        (new Greet()).greetings();
     }
 }
